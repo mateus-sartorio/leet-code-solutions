248c4c8d0a0c84653f01efe9b1ef5df28f263d19
Mateus <69646100+mateus-sartorio@users.noreply.github.com>
2024-01-26 16:18:29 -0300

    Create evaluate_reverse_polish_notation.cpp

---
+++ evaluate_reverse_polish_notation.cpp
@@ -0,0 +1,51 @@
+// 150. Evaluate Reverse Polish Notation
+// Leet code problem: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+#include <stack>
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        std::stack<int> stack;
+
+        for(auto& token : tokens) {
+            if(!token.compare("+")) {
+                int v1 = stack.top();
+                stack.pop();
+                int v2 = stack.top();
+                stack.pop();
+                int result = v2 + v1;
+                stack.push(result);
+            }
+            else if(!token.compare("-")) {
+                int v1 = stack.top();
+                stack.pop();
+                int v2 = stack.top();
+                stack.pop();
+                int result = v2 - v1;
+                stack.push(result);
+            }
+            else if(!token.compare("*")) {
+                int v1 = stack.top();
+                stack.pop();
+                int v2 = stack.top();
+                stack.pop();
+                int result = v2 * v1;
+                stack.push(result);
+            }
+            else if(!token.compare("/")) {
+                int v1 = stack.top();
+                stack.pop();
+                int v2 = stack.top();
+                stack.pop();
+                int result = v2 / v1;
+                stack.push(result);
+            }
+            else {
+                stack.push(stoi(token));
+            }
+        }
+
+        return stack.top();
+    }
+};
